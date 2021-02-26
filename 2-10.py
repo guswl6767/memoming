f3683b6ec49953290f529e0e36df2b3518d375c2
guswl6767 <guswl6767@naver.com>
2021-02-26 13:36:17 +0900

    python

---
+++ 2-10.py
@@ -0,0 +1,26 @@
+import sys
+#sys.stdin=open("input.txt", "r")
+n = (input())
+a= (input())
+b= (input())
+c= (input())
+d= (input())
+e= (input())
+def hwemon(x):
+    
+    if x.lower() == x.lower()[::-1]:
+        print("YES")
+    else:
+        print("NO")
+    return
+
+hwemon(a)
+hwemon(b)
+hwemon(c)
+hwemon(d)
+hwemon(e)
+
+
+
+     
+
